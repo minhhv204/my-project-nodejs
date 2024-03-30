d2b556c95298c9e041dc85272c386bb165078959
minhhv24 <hminh0555@gmail.com>
2024-03-30 13:58:28 +0700

    cuoi cung

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,7 +5,9 @@ export const connectMongoDB = async (DB_URI) =>{
   try {
     // use mongoose: npm i mongoose
     //mongodb://127.0.0.1:27017/db_name
-    await mongoose.connect("mongodb+srv://minhhv204:0394494851@cluster0.bdiders.mongodb.net/db_nodejs?retryWrites=true&w=majority&appName=Cluster0");
+    //mongodb+srv://hminh0555:<password>@cluster0.hkor2zg.mongodb.net/
+    //mongodb+srv://hminh0555:0394494851@cluster0.hkor2zg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+    await mongoose.connect("mongodb+srv://hminh0555:0394494851@cluster0.hkor2zg.mongodb.net/db_nodejs?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Connect successfully!!!");
   } catch (error) {
     console.log("Connect failure!!!");
